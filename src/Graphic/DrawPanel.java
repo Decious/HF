85d738ab92e5cf0048290e7da87cfaf91e40b069
Dmitriy <retroman142>
2019-03-03 19:39:07 +0300

    graphics gradient

--- src/Graphic/DrawPanel.java
+++ src/Graphic/DrawPanel.java
@@ -11,19 +11,28 @@ public class DrawPanel extends JPanel {
 //        }
 
 
-    public void paintComponent(Graphics graphics) {
+//    public void paintComponent(Graphics graphics) {
+//
+//        graphics.fillRect(0,0,this.getWidth(),this.getHeight());
+//
+//        int red= (int) (Math.random()*255);
+//        int green= (int) (Math.random()*255);
+//        int blue= (int) (Math.random()*255);
+//
+//        Color randColor = new Color(red, green, blue);
+//        graphics.setColor(randColor);
+//        graphics.fillOval(70,70,100,50);
+//
+//    }
 
-        graphics.fillRect(0,0,this.getWidth(),this.getHeight());
 
-        int red= (int) (Math.random()*255);
-        int green= (int) (Math.random()*255);
-        int blue= (int) (Math.random()*255);
+        public void paintComponent (Graphics g){
+            Graphics2D g2d = (Graphics2D) g;
+            GradientPaint gradient = new GradientPaint(70, 70, Color.RED,
+                    150, 150, Color.CYAN);
+            g2d.setPaint(gradient);
+            g2d.fillOval(70,70 ,100,100);
 
-        Color randColor = new Color(red, green, blue);
-        graphics.setColor(randColor);
-        graphics.fillOval(70,70,100,50);
-
-    }
-
+        }
 
 }
