83b166fe4c00ce708d805fb9125a37f6dd1b3450
Dmitriy <retroman142>
2019-03-03 19:02:12 +0300

    g1

---
+++ src/Graphic/DrawPanel.java
@@ -0,0 +1,28 @@
+package Graphic;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class DrawPanel extends JPanel {
+//        public void paintComponent(Graphics graphics){
+//            graphics.setColor(Color.blue);
+//            graphics.fillRect(20, 50, 100, 100);
+//        }
+
+
+    public void paintComponent(Graphics graphics) {
+
+        graphics.fillRect(0,0,this.getWidth(),this.getHeight());
+
+        int red= (int) (Math.random()*255);
+        int green= (int) (Math.random()*255);
+        int blue= (int) (Math.random()*255);
+
+        Color randColor = new Color(red, green, blue);
+        graphics.setColor(randColor);
+        graphics.fillOval(70,70,100,100);
+    }
+
+
+}
--- src/Graphic/SimpleGui.java
+++ src/Graphic/SimpleGui.java
@@ -1,32 +1,38 @@
 package Graphic;
 
+import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
 
-public class SimpleGui implements ActionListener{
+public class SimpleGui implements ActionListener {
     JButton button;
+    JFrame frame;
+
     public static void main(String[] args) {
         SimpleGui gui = new SimpleGui();
         gui.go();
     }
-    public void go(){
 
-        JFrame frame = new JFrame();
+    public void go() {
+        frame = new JFrame();
+
         button = new JButton("Click me!");
+        button.setSize(50,10);
 
         button.addActionListener(this);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(button);
-        frame.setSize(300,300);
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+        frame.setSize(300, 300);
         frame.setVisible(true);
 
     }
 
 
     public void actionPerformed(ActionEvent e) {
-        button.setText("I've been clicked");
+        button.setText("I've been clicked!");
 
     }
+
 }
