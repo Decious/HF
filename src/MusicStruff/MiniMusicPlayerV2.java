a0412a9ec2ec57f3ec4accb873cc6b58f9928899
Dmitriy <workmail1421@gmail.com>
2019-03-05 11:29:57 +0300

    music player V2

--- src/MusicStruff/MiniMusicPlayerV2.java
+++ src/MusicStruff/MiniMusicPlayerV2.java
@@ -2,17 +2,26 @@ package MusicStruff;
 
 import javax.sound.midi.*;
 
-public class MiniMusicPlayer {
+public class MiniMusicPlayerV2 implements ControllerEventListener {
+
     public static void main(String[] args) {
+        MiniMusicPlayerV2 mini = new MiniMusicPlayerV2();
+        mini.go();
+    }
+
+    public void go() {
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
 
+            int[] eventsIWant = {127};
+            sequencer.addControllerEventListener(this, eventsIWant);
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
 
             for (int i = 5; i < 61; i++) {
                 track.add(Util.makeEvent(144, 1, i, 100, i));
+                track.add(Util.makeEvent(176, 1, 127, 0, i));
                 track.add(Util.makeEvent(128, 1, i, 100, i + 2));
             }
 
@@ -22,5 +31,10 @@ public class MiniMusicPlayer {
         } catch (MidiUnavailableException | InvalidMidiDataException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void controlChange(ShortMessage event) {
+        System.out.println("Ля");
     }
 }
---
+++ src/MusicStruff/Util.java
@@ -0,0 +1,20 @@
+package MusicStruff;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+
+public class Util {
+    public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
+        return event;
+    }
+}
