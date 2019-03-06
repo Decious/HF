f8216305757fa8d2c7f3adbb46109b9302f244ab
Dmitriy <workmail1421@gmail.com>
2019-03-06 23:30:54 +0300

    FileWriterTest

---
+++ src/IOSTUFF/WriteAFile.java
@@ -0,0 +1,18 @@
+package IOSTUFF;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteAFile {
+    public static void main(String[] args) {
+        FileWriter  filewriter = null;
+        try {
+            filewriter = new FileWriter("Test.txt");
+            filewriter.write("FILEWRITER TEST");
+            filewriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
