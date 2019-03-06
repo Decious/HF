bbfea4d47888a0525fce64f973553e9aecf333a3
Dmitriy <workmail1421@gmail.com>
2019-03-06 18:50:16 +0300

    TextArea

---
+++ src/Graphic/TextArea.java
@@ -0,0 +1,42 @@
+package Graphic;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextArea implements ActionListener {
+
+    JTextArea text;
+
+    public static void main(String[] args) {
+        TextArea gui = new TextArea();
+        gui.go();
+    }
+
+    private void go() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Just click it!");
+        button.addActionListener(this);
+        text = new JTextArea(10,20);
+        text.setLineWrap(true);
+
+        JScrollPane scrollBar = new JScrollPane(text);
+        scrollBar.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollBar.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        panel.add(scrollBar);
+
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.setSize(280, 270);
+        frame.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        text.append("Button clicked \n ");
+    }
+}
