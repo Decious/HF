183afc479e19d1552bbd739141876552115a61fe
Dmitriy <workmail1421@gmail.com>
2019-03-08 15:30:08 +0300

    very simple chat client

---
+++ src/net/SimpleChatClient.java
@@ -0,0 +1,57 @@
+package net;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SimpleChatClient {
+    JTextField outgoing;
+    PrintWriter writer;
+    Socket socket;
+
+    public void go(){
+        JFrame frame = new JFrame("Very Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        outgoing = new JTextField();
+        JButton sendButton = new JButton("SendIt");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        setUpNetWorking();
+        frame.setSize(400,500);
+        frame.setVisible(true);
+    }
+
+    private void setUpNetWorking() {
+        try{
+            socket = new Socket("127.0.0.1" , 5000);
+            writer = new PrintWriter(socket.getOutputStream());
+            System.out.println("networking established!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class SendButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try{
+                writer.println(outgoing.getText());
+                writer.flush();
+            }catch (Exception e1){e1.printStackTrace();}
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
+    }
+
+    public static void main(String[] args) {
+        new SimpleChatClient().go();
+    }
+}
