4bacab0f76a6a22a1e35807f71da925af3c06ea1
Dmitriy <workmail1421@gmail.com>
2019-03-09 16:00:01 +0300

    simple chat client updated

--- src/net/SimpleChatClient.java
+++ src/net/SimpleChatClient.java
@@ -4,33 +4,62 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class SimpleChatClient {
+    JTextArea incoming;
     JTextField outgoing;
     PrintWriter writer;
     Socket socket;
+    BufferedReader reader;
+    static String name;
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name1) {
+        name = name1;
+    }
 
     public void go() {
+
         JFrame frame = new JFrame("Very Simple Chat Client");
         JPanel mainPanel = new JPanel();
-        outgoing = new JTextField(20);
+        incoming = new JTextArea(15, 30);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        outgoing = new JTextField(25);
         JButton sendButton = new JButton("SendIt");
         sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(qScroller);
         mainPanel.add(outgoing);
         mainPanel.add(sendButton);
-        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         setUpNetWorking();
-        frame.setSize(400, 500);
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(390, 320);
         frame.setVisible(true);
     }
 
     private void setUpNetWorking() {
         try {
             socket = new Socket("127.0.0.1", 5000);
+            InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+            reader = new BufferedReader(isr);
+
             writer = new PrintWriter(socket.getOutputStream());
             System.out.println("networking established!");
         } catch (IOException e) {
@@ -54,6 +83,25 @@ public class SimpleChatClient {
     }
 
     public static void main(String[] args) {
-        new SimpleChatClient().go();
+        SimpleChatClient client = new SimpleChatClient();
+        System.out.println("Pickup the nickname");
+        Scanner scanner = new Scanner(System.in);
+        setName(scanner.nextLine());
+        client.go();
+    }
+
+    private class IncomingReader implements Runnable {
+        @Override
+        public void run() {
+            String msg;
+            try {
+                while ((msg = reader.readLine()) != null) {
+                    System.out.println("read " + msg);
+                    incoming.append(name+": "+msg + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
